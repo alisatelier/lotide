b14cabaf66a6f8dbe1fa817e497fc47655f05b06
alisatelier <[194897761+alisatelier@users.noreply.github.com]>
2025-07-06 00:52:14 -0600

    refactored middle.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,11 @@
+const eqArrays = require("./eqArrays");
 
-const eqArrays = require("./eqArrays")
-
-const assertArraysEqual = function (actual, expected) {
+const assertArraysEqual = function(actual, expected) {
   if (eqArrays(actual, expected)) {
     console.log(`✅ Assertion Passed ${actual.toString()} === ${expected.toString()}`);
   } else {
     console.log(`🛑 Assertion Failed: ${actual.toString()} !== ${expected.toString()}`);
   };
-     } ;
+};
 
 module.exports = assertArraysEqual;
--- middle.js
+++ middle.js
@@ -1,26 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-    return false;
-  }
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed ${actual.toString()} === ${expected.toString()}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual.toString()} !== ${expected.toString()}`);
-  };
-};
 
 const middle = function(arr) {
   const lengthCheck = arr.length;
@@ -32,9 +9,5 @@ const middle = function(arr) {
   else return [arr[middleIndex]];
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+
+module.exports = middle;
