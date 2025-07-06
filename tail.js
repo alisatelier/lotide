cf52836a83f70052396613e5ecaeb39be521f973
alisatelier <[194897761+alisatelier@users.noreply.github.com]>
2025-07-06 00:22:03 -0600

    refactored tail.js to modularize

--- tail.js
+++ tail.js
@@ -2,29 +2,4 @@ const tail = function (array) {
   return array.slice(1);
 };
 
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const newArray = tail(words);
-assertEqual(words.length, 3);
-
-console.log(newArray);
-console.log(words);
-
-assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
-assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
-
-assertEqual(newArray.length, words.length - 1); // tail should have one element less than its input
-
-assertEqual(newArray.length, words.length - 0); // tail should have one element less than its input
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,20 @@
+const tail = require("../tail")
+const assertEqual = require("../assertEqual")
+
+const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const newArray = tail(words);
+assertEqual(words.length, 3);
+
+console.log(newArray);
+console.log(words);
+
+assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
+assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
+
+assertEqual(newArray.length, words.length - 1); // tail should have one element less than its input
+
