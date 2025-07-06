f42ad2c9e533479f621fbb3f96ff524b87980e80
alisatelier <[194897761+alisatelier@users.noreply.github.com]>
2025-07-06 00:30:59 -0600

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const eqArrays = function(arr1, arr2){
   if (arr1.length !== arr2.length) {
@@ -18,8 +11,5 @@ const eqArrays = function(arr1, arr2){
   }
     return true;
 }
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
