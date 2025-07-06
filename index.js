0cff45e23685b14263ff59ae1a09f28c0c4507df
alisatelier <[194897761+alisatelier@users.noreply.github.com]>
2025-07-06 14:35:06 -0600

    referenced library

---
+++ index.js
@@ -0,0 +1,36 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
+const eqObjects = require('./eqObjects')
+const assertObjectsEqual = require('./assertObjectsEqual')
+const countLetters = require('./countLetters')
+const countOnly = require('./countOnly')
+const findKey = require('./findKey')
+const findKeyByValue = require('./findKeyByValue')
+const flatten = require('./flatten')
+const letterPositions = require('./letterPositions')
+const map = require('./map')
+const takeUntil = require('./takeUntil')
+const without = require('./without')
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual,
+  eqObjects: eqObjects,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
+
+};
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,3 @@
-// const tail = require("../tail")
-// const assertEqual = require("../assertEqual")
-
-// const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
-
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// const newArray = tail(words);
-// assertEqual(words.length, 3);
-
-// assertEqual(tail(["test"]).length, 0); // tail of array with one element only should be empty
-// assertEqual(tail([]).length, 0); // tail of an empty array should also be empty
-
-// assertEqual(newArray.length, words.length - 1); // tail should have one element less than its input
 
 const assert = require('chai').assert;
 const tail  = require('../tail');
