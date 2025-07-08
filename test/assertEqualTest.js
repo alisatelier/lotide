9e683bb4a70bfda808eea49c044bf45776a9bd6f
alisatelier <[194897761+alisatelier@users.noreply.github.com]>
2025-07-07 18:13:50 -0600

    commented out non-mocha/chai testing

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,7 @@
-const assertEqual = require("../assertEqual")
+const assertEqual = require("../assertEqual");
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
-assertEqual("Same", "Same");
-assertEqual("Same", "Different");
+// assertEqual("Same", "Same");
+// assertEqual("Same", "Different");
